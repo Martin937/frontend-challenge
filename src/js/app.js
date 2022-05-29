21500c6ff995f0e5576e435d25b2f870054c42a1
Martin9371 <Martin937@yandex.ru>
2022-05-29 15:48:46 +0300

    finish layout

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from './modules/functions.js';
+
+flsFunctions.isWebp();
+
+console.log('Hello gulp');
+
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+const swiper = new Swiper()
